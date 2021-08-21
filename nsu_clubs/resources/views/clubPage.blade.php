56aa33e0395a97df3c309d7e1bd69c921d748e22
Tazin <45015333+TazinMorshed@users.noreply.github.com>
2021-08-21 12:11:13 +0600

    added Edit modal in Club page

--- nsu_clubs/resources/views/clubPage.blade.php
+++ nsu_clubs/resources/views/clubPage.blade.php
@@ -119,7 +119,7 @@
 							<div class="mu-title-area">
 						
 								<h2 class="mu-title">Club Details
-								<button type="button" id="editButton" class="btn btn-dark">
+								<button type="button" id="editButton" class="btn btn-dark"  data-toggle="modal" data-target="#modalCart">
 								<i class="fa fa-wrench" aria-hidden="true"></i> 	
 								Edit</button>
 								</h2>
@@ -165,7 +165,44 @@
 		<!-- End Schedule -->
 
 
-  
+  <!-- Modal -->
+  <div class="modal fade" id="modalCart" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Make Changes</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+	  
+                    <div class="form-group">
+                        <label for="message-text" class="col-form-label">Change Club Description</label>
+                        <textarea class="form-control" id="message-text"></textarea>
+                    </div>
+					<div class="form-group">
+					<label class="form-label" for="customFile">Upload logo</label>
+					<input type="file" class="form-control" id="customFile" />
+					</div>
+					<div class="form-group">
+					<label class="form-label" for="customFile">Upload Background Image</label>
+					<input type="file" class="form-control" id="customFile" />
+					</div>
+                 
+                    
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
 
   <!-- Modal -->
   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
